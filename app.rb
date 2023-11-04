2e8144042c847820c0b44d8690455406b08b80e3
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-11-04 15:17:26 +0000

    added square root

--- app.rb
+++ app.rb
@@ -7,3 +7,27 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("user's_number").to_f
+
+  @the_result = @the_num ** 2
+  
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("user's_number").to_f
+
+  @the_result = @the_num ** 0.5
+  
+  erb(:square_root_results)
+end
