4c3115d2da1420cc43f384cfb5d84025fe5f5b17
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-11-04 15:48:26 +0000

    added payment

--- app.rb
+++ app.rb
@@ -31,3 +31,22 @@ get("/square_root/results") do
   
   erb(:square_root_results)
 end
+
+get("/payment/new") do
+  erb(:payment_calc)
+end
+
+get("/payment/results") do
+  @the_num = params.fetch("user's_number").to_f
+  @the_num_2 = params.fetch("user's_number_2").to_f
+  @the_num_3 = params.fetch("user's_number_3").to_f
+  r = @the_num/100/12
+  n = @the_num_2.to_i * 12
+  pv = @the_num_3
+  
+  @numerator = r * pv
+  @denominator = 1 - ((1 + r) ** -n)
+  @the_result = @numerator/@denominator
+  
+  erb(:payment_results)
+end
